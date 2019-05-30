0717dd93ea45d15054752c1a7ccb624ab55f47c7
mnezmah <mario.nezmah@gmail.com>
2019-05-30 18:16:46 +0200

    Set up form validation

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -3,12 +3,12 @@ function submitComment() {
   const name = inputField.value
   const textArea = document.getElementById('msg')
   const message = textArea.value
- 
+
   //creating new section
   const comment = document.createElement('section')
   const heading = document.createElement('h3')
   const paragraph = document.createElement('p')
-  
+
   //adjusting newly created section
   heading.innerHTML = `${name} said:`
   paragraph.innerHTML = message
@@ -24,5 +24,16 @@ function submitComment() {
   //clearing the form
   inputField.value = null
   textArea.value = null
+
+  //error messages
+  if (!name || !message) {
+    alert("Please fill in both, your name and the message")
+    return null
+  }
+  if (message.lenght > 280) {
+    alert('your message is too long')
+    return true
+  }
+  return false
 }
 
